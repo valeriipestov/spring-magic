d1b5706114cd3ad7081c560ea0cfc91e677b2d6e
valeriyPestov <valeriy.pestov@yahoo.com>
2022-07-17 21:47:30 +0300

    initial commit

---
+++ src/main/java/com/vpestov/training/springmagic/SpringMagicApplication.java
@@ -0,0 +1,13 @@
+package com.vpestov.training.springmagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMagicApplication.class, args);
+    }
+
+}
